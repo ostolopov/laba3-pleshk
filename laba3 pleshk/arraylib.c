a7029880c42ba6fdcc3c1f7ba865cd3f77652531
ostolopov <egor21231bar@gmail.com>
2024-12-15 19:36:43 +0300

    update

---
+++ laba3 pleshk/arraylib.c
@@ -0,0 +1,8 @@
+//
+//  arraylib.c
+//  laba3 pleshk
+//
+//  Created by Egor Baranov on 15.12.2024.
+//
+
+#include "arraylib.h"
---
+++ laba3 pleshk/arraylib.h
@@ -0,0 +1,13 @@
+//
+//  arraylib.h
+//  laba3 pleshk
+//
+//  Created by Egor Baranov on 15.12.2024.
+//
+
+#ifndef arraylib_h
+#define arraylib_h
+
+#include <stdio.h>
+
+#endif /* arraylib_h */
