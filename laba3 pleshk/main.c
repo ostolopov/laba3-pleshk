c3a5dbccf3b988362f372f83293a80017a13e635
ostolopov <egor21231bar@gmail.com>
2024-12-15 18:15:32 +0300

    Initial Commit

---
+++ laba3 pleshk/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  laba3 pleshk
+//
+//  Created by Egor Baranov on 15.12.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
